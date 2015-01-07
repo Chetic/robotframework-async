c5b165f1e4b2dac85b4bb5d9f47da42291c7d957
Chetic <Chetic@gmail.com>
2015-01-07 13:28:05 +0100

    Added project to Python Package Index

---
+++ robotframework-async/async.py
@@ -0,0 +1,49 @@
+import sys
+import os
+import time
+from robot.libraries.BuiltIn import BuiltIn
+from robot.output import librarylogger as log
+from robot.running import Keyword
+from robot.running.context import EXECUTION_CONTEXTS
+
+class async:
+    def __init__(self):
+        self._thread_pool = {}
+        self._last_thread_handle = 0
+
+    def async_run(self, keyword, *args, **kwargs):
+        ''' Executes the provided Robot Framework keyword in a separate thread and immediately returns a handle to be used with async_get '''
+        handle = self._last_thread_handle
+        thread = self._threaded(keyword, *args, **kwargs)
+        thread.start()
+        self._thread_pool[handle] = thread
+        self._last_thread_handle += 1
+        return handle
+
+    def async_get(self, handle):
+        ''' Blocks until the thread created by async_run returns '''
+        assert handle in self._thread_pool, 'Invalid async call handle'
+        result = self._thread_pool[handle].result_queue.get()
+        del self._thread_pool[handle]
+        return result
+
+    def _get_handler_from_keyword(self, keyword):
+        ''' Gets the Robot Framework handler associated with the given keyword '''
+        if EXECUTION_CONTEXTS.current is None:
+            raise RobotNotRunningError('Cannot access execution context')
+        return EXECUTION_CONTEXTS.current.get_handler(keyword)
+
+    def _threaded(self, keyword, *args, **kwargs):
+        import Queue
+        import threading
+        
+        def wrapped_f(q, *args, **kwargs):
+            ''' Calls the decorated function and puts the result in a queue '''
+            f = self._get_handler_from_keyword(keyword)
+            ret = f.run(EXECUTION_CONTEXTS.current, args)
+            q.put(ret)
+
+        q = Queue.Queue()
+        t = threading.Thread(target=wrapped_f, args=(q,)+args, kwargs=kwargs)
+        t.result_queue = q
+        return t
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name = "robotframework-async",
+    version = "1.0.0",
+    description = "Generic Robot Framework library for asynchronous keyword execution",
+    author = "Fredrik Reveny",
+    author_email = "chetic@gmail.com",
+    url = "https://github.com/Chetic/robotframework-async",
+    download_url = "http://chardet.feedparser.org/download/python3-chardet-1.0.1.tgz",
+    keywords = ["async", "robotframework"],
+    install_requires = ["robotframework >= 2.8.6"],
+    packages = ["robotframework-async"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ]
+)
