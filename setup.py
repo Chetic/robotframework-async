71381cb4223f0789198b1c2826b126d4aafb8db6
Chetic <Chetic@gmail.com>
2015-01-07 14:41:37 +0100

    Added a license

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "robotframework-async",
-    version = "1.0.1",
+    version = "1.0.3",
     description = "Generic Robot Framework library for asynchronous keyword execution",
     author = "Fredrik Reveny",
     author_email = "chetic@gmail.com",
